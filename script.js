6062b21a52821ece6861dd911af6f0c1fd9d5937
ayatsakalla <ayat.sakalla@outlook.com>
2024-01-23 14:55:19 -0500

    started javascript

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 // Function to calculate and display Tip, Total Bill, and Amt per Person
 function calculateAndDisplay(){
+    // get values from HTML
+    let bill = parseFloat (document.getElementById('totalBill').value)
+    let numOfPeople = parseInt(document.getElementById('numOfPeople').value)
+    let serviceQuality = document.getElementById('serviceQuality').value
 
+    // calculate tip, total Bill, and amt each person owes
+    let tip = calculateTip(bill, serviceQuality)
+    let totalBill = calculateTotalBill (bill, tip)
+    let amtPerPerson = calcAmtPerPerson (totalBill, numOfPeople)
+
+    // display results in HTML
+    document.getElementById('tipResult').innerText = 'Tip: $' + tip.toFixed(2)
+    document.getElementById('totalBillResult').innerText = 'Bill: $' + totalBill.toFixed(2)
+    document.getElementById('amtPerPersonResult').innerText = 'Total Per Person:' + amtPerPerson.toFixed(2)
 }
